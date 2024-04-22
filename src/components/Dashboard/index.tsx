f959f331871a4ce570fc77ab376c340b3c59b6c4
Pooya Golchian <pooya.golchian@gmail.com>
2024-04-22 10:38:21 +0400

    🔄 Refactor DrawerMenu component and AppBar styling

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,19 +1,14 @@
-import { useNavigate } from 'react-router-dom';
 import MainLayout from "../MainLayout";
 
 const Dashboard = () => {
-    const navigate = useNavigate();
-
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        navigate('/');
-    };
+    // const navigate = useNavigate();
+    // const handleLogout = () => {
+    //     localStorage.removeItem('token');
+    //     navigate('/');
+    // };
 
     return (
-        <MainLayout>
-               <h1>Dashboard</h1>
-               <button onClick={handleLogout}>Logout</button>
-        </MainLayout>
+        <MainLayout />
     );
 };
 
--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,41 +1,11 @@
-import React, {ReactNode, useState} from 'react';
-import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import DrawerComponent from '../DrawerMenu';
+import DrawerMenu from '../DrawerMenu';
 
-// Define an interface for the component props
-interface MainLayoutProps {
-    children: ReactNode; // ReactNode includes everything that can be rendered: React elements, strings, numbers, fragments, portals, etc.
-}
 
-const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-    const [drawerOpen, setDrawerOpen] = useState(false);
-
-    const toggleDrawer = (open: boolean) => {
-        setDrawerOpen(open);
-    };
-
+const MainLayout = () => {
     return (
         <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        onClick={() => toggleDrawer(true)}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" style={{ flexGrow: 1 }}>
-                        Dashboard
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-            <DrawerComponent open={drawerOpen} toggleDrawer={toggleDrawer} />
-            <main className="p-4">
-                {children}
-            </main>
+            <DrawerMenu />
+            <main className="p-4"/>
         </div>
     );
 };
