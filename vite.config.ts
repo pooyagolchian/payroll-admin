468dcc3ecbb1629ecb000a55311ce3234224d3b7
Pooya Golchian <pooya.golchian@gmail.com>
2024-04-24 20:16:41 +0400

    🔄 Improve Vite build command in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-  },
-  build: {
-    outDir: 'build',
-  },
+  base: '/',
 });
